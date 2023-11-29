b91be27d3e8c6312ebaf9c2eb662d72aff438106
alalhossain94 <alalhossain557@gmail.com>
2023-11-30 03:00:33 +0600

    description added

--- Module 11.5 Practice day 02/project_meal/meal/views.py
+++ Module 11.5 Practice day 02/project_meal/meal/views.py
@@ -8,52 +8,62 @@ def meal_list(request):
         {
             "strMeal": "BeaverTails",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
-            "idMeal": "52928"
+            "idMeal": "52928",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Breakfast Potatoes",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
-            "idMeal": "52965"
+            "idMeal": "52965",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Canadian Butter Tarts",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
-            "idMeal": "52923"
+            "idMeal": "52923",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Montreal Smoked Meat",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
-            "idMeal": "52927"
+            "idMeal": "52927",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Nanaimo Bars",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
-            "idMeal": "52924"
+            "idMeal": "52924",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Pate Chinois",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
-            "idMeal": "52930"
+            "idMeal": "52930",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Pouding chomeur",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
-            "idMeal": "52932"
+            "idMeal": "52932",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Poutine",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
-            "idMeal": "52804"
+            "idMeal": "52804",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Rappie Pie",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
-            "idMeal": "52933"
+            "idMeal": "52933",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         },
         {
             "strMeal": "Split Pea Soup",
             "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
-            "idMeal": "52925"
+            "idMeal": "52925",
+            "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
         }
     ]
 
